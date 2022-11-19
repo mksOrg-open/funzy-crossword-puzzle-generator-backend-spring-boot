55dc6f06252e1b1f78b63a1d0a1528d8baf488b6
Akshit Batra <akshit.batra@thoughtworks.com>
2022-11-19 16:36:27 +0530

    Akshit: Setup. Initial Commit

---
+++ src/main/java/com/mykraftyskills/Funzy/FunzyApplication.java
@@ -0,0 +1,13 @@
+package com.mykraftyskills.Funzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mykraftyskills/Funzy/FunzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykraftyskills.Funzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
